dea20c94e75499980611a0252807479dfd5bc56f
Tutch <lucasmapurunga@gmail.com>
2016-11-11 00:38:42 -0300

    Agora o arduino faz uma média das ultimas leituras antes de escrever na Serial

--- arduino/Smartlab_Megazord/Smartlab_Megazord.ino
+++ arduino/Smartlab_Megazord/Smartlab_Megazord.ino
@@ -7,40 +7,99 @@ IRsend irsend;
 #define DHTPIN 6
 #define echoPin 2
 #define trigPin 3
+#define TIME_TO_READ_SENSORS 30
+#define READ_SENSORS_DELAY 5000
 
 DHT dht(DHTPIN, DHTTYPE);
 int LDR_Pin = A0; //analog pin 0
 
-// codigo do ar Carrier dos laboratorios
+// RAW code for AC shutdown
 unsigned int irSignal[] = {4236, 4488, 396, 1764, 396, 648, 416, 1760, 396, 1764, 392, 652, 416, 648, 416, 1740, 416, 652, 416, 648, 416, 1760, 396, 648, 416, 652, 412, 1744, 412, 1764, 396, 648, 416, 1764, 392, 652, 416, 1760, 396, 1764, 392, 1764, 392, 1768, 392, 648, 416, 1760, 396, 1764, 392, 1760, 396, 652, 416, 648, 420, 648, 416, 648, 416, 1764, 392, 652, 412, 652, 416, 1740, 416, 1764, 392, 1764, 392, 652, 420, 644, 420, 648, 416, 648, 416, 652, 416, 648, 416, 648, 420, 648, 416, 1760, 396, 1764, 396, 1760, 396, 1736, 420, 1760, 396, 5296, 4244, 4484, 396, 1764, 396, 648, 416, 1764, 392, 1764, 392, 652, 416, 648, 416, 1760, 396, 648, 416, 652, 416, 1760, 396, 648, 416, 652, 416, 1760, 396, 1760, 396, 648, 416, 1764, 396, 648, 416, 1760, 396, 1760, 396, 1764, 392, 1764, 396, 648, 416, 1760, 396, 1740, 416, 1764, 392, 652, 416, 652, 412, 652, 416, 648, 416, 1760, 396, 652, 416, 648, 416, 1760, 396, 1764, 392, 1764, 392, 652, 416, 652, 412, 652, 416, 648, 416, 648, 420, 648, 416, 652, 412, 652, 412, 1764, 396, 1760, 396, 1740, 416, 1764, 396, 1760, 396, 5712, 72};
 int khz = 38; // 38kHz carrier frequency for the NEC protocol
 
 String messageReceived;
 const String TURN_OFF = "shutdown";
 
-boolean waitForContent = true;
-int loopCount = 0;
+unsigned long sendStart;
+unsigned long loopTime;
+
+unsigned int timesRead = 0;
+
+// Sensor data
+long duration, distance;
+int LDRReading;
+float temperature;
+bool presence = false;
+
+// Mediam from readings
+long mediamDistance;
+long mediamLDR;
+float mediamTemperature;
+String mediamPresence = "false";
 
 
 void setup() {
   Serial.begin(9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  dht.begin();
+  dht.begin();  
 }
 
 void loop() {
-  long duration, distance;
-  String presence = "false";
-   
-  int LDRReading = analogRead(LDR_Pin); 
-  float t = dht.readTemperature();
 
-  if (isnan(t)) {
+  // Read data in cicles before transmitting content
+  readSensors();
+
+  mediamDistance += distance;
+  mediamLDR += LDRReading;
+  mediamTemperature += temperature;
+
+  // If a single presence check is true, the resulting presence is true.
+  if(presence == true){
+    mediamPresence = "true";
+  }
+  
+  timesRead++;
+
+  // Calculate mediam and send data
+  if(timesRead >= ((TIME_TO_READ_SENSORS/(READ_SENSORS_DELAY/1000))+1 )){
+    mediamDistance /= timesRead;
+    mediamLDR /= timesRead;
+    mediamTemperature /= timesRead;
+    
+    writeSensorsToSerial();
+
+    timesRead = 0;
+    mediamDistance = 0;
+    mediamLDR = 0;
+    mediamTemperature = 0;
+  }
+
+  sendStart = millis();
+
+  while(loopTime < READ_SENSORS_DELAY){
+     // Checks serial data to shutdown the AC    
+    if (Serial.available() > 0){
+      String message = Serial.readString();
+      turnOffAC(message); 
+    }
+
+    loopTime = millis() - sendStart;
+  }
+
+  loopTime = 0; 
+}
+
+// Read sensors connected to the arduino board.
+void readSensors(){
+  LDRReading = analogRead(LDR_Pin); 
+  temperature = dht.readTemperature();
+  
+  if (isnan(temperature)) {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
-
+  
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
@@ -48,48 +107,39 @@ void loop() {
   digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH);
   distance = (duration/2) / 29.1;
-
+  
   if(distance < 160){
-    presence = "true";
+    presence = true;
   }
+}
 
+// Write the mediam of data measured by sensors to the serial port.
+void writeSensorsToSerial(){
   Serial.print("Temperatura=");
-  Serial.print(t);
+  Serial.print(mediamTemperature);
   Serial.print(";");
   Serial.print("Luz=");
-  Serial.print(LDRReading);
+  Serial.print(mediamLDR);
   Serial.print(";");
   Serial.print("Pres=");
-  Serial.print(presence);
+  Serial.print(mediamPresence);
   Serial.print(";");
   Serial.print("Proximidade=");
-  Serial.print(distance);
+  Serial.print(mediamDistance);
   Serial.println("");
+}
 
-  while(waitForContent){
-          Serial.println(loopCount);
-
-    // send data only when you receive data:
-    if (Serial.available() > 0) {
-      // read the incoming byte:
-      messageReceived = Serial.read();
-    
-      if(messageReceived.equals(TURN_OFF)){
+void turnOffAC(String messageReceived){
+  Serial.println("-- Desligamento --");
+  
+  // read the incoming byte:
+  if(messageReceived.equals(TURN_OFF)){
+    for(int j = 0; j < 3; j++){    
         for (int i = 0; i < 3; i++) {
           irsend.sendRaw(irSignal, sizeof(irSignal) / sizeof(irSignal[0]), khz); //Note the approach used to automatically calculate the size of the array.
         }
-      }
-    }
-
-    delay(1000);
-    loopCount++;
-
-    if(loopCount == 10){
-      waitForContent = false;
+      delay(100);
     }
   }
-
-  waitForContent = true;
-  loopCount = 0;
-
+  
 }
